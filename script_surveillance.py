6cc818b6f25071f6730488b2e6473c39b0b5509b
tangara0559 <j.p.lejeune25@gmail.com>
2026-09-07 09:11:56 +0200

    Change MOT_CLE from 'Marseille' to 'Montpellier'

--- script_surveillance.py
+++ script_surveillance.py
@@ -6,7 +6,7 @@ import os
 
 # Configuration
 URL = "https://france.diplomatie.belgium.be/fr/services-consulaires/services-consulaires-marseille/missions-flying-kit-marseille"
-MOT_CLE = "Marseille"
+MOT_CLE = "Montpellier"
 
 # Configuration Email (via variables d'environnement pour la sécurité)
 EMAIL_EMETTEUR = os.environ.get("EMAIL_EMETTEUR")
